d8ed61623a5c0a50df04a6a2c4fa7801bd499081
Natalya Kadikina <natalya92filipp@mail.ru>
2022-07-09 18:14:14 +0300

    Добавила System.out.println("Test dz1brunch")

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,8 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Test dz1brunch");
+
 	// write your code here
         //Test dz1brunch
     }
